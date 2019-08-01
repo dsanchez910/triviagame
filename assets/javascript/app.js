d5fffb4978a014f0d7fa78a0f02a5d4e0ce405e9
Danny Sanchez <dsanchez910@gmail.com>
2019-07-31 22:17:33 -0400

    added questions for the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+const triviaQuestions = [{
+	question: "How many World Series titles do the Yankees have?",
+	answerList: ["9", "16", "22", "27"],
+	answer: 3
+},{
+	question: "Who was the last Yankees Player to win Rookie of the Year?",
+	answerList: ["Derek Jeter", "Mariano Rivera", "Aaron Judge", "Alex Rodriguez"],
+	answer: 2
+},{
+	question: "Who was the last official Yankees Captain?",
+	answerList: ["Derek Jeter", "Mariano Rivera", "Aaron Judge", "Alex Rodriguez"],
+	answer: 0
+},{
+	question: "What year did the Yankees capture their first World Series?",
+	answerList: ["1903","1927", "1965", "1991"],
+	answer: 1
+},{
+	question: "What city in New York do the Yankees play in?",
+	answerList: ["Queens", "Brooklyn", "Manhattan", "Bronx"],
+	answer: 3
+},{
+	question: "What Team is the Yankees biggest Rival?",
+	answerList: ["Dodgers", "Mets", "Red Sox", "Rays"],
+	answer: 3
+},{
+	question: "Who was the last Yankees pitcher to win World Series MVP?",
+	answerList: ["CC Sabathia", "Rodger Clemmons", "Mariano Rivera", "David Cone"],
+	answer: 2
+},{
+	question: "What year was the last Yankees World Series Title?",
+	answerList: ["2001", "2005", "2009", "2011"],
+	answer: 2
+},{
+	question: "What year was the new Yankees Stadium built?",
+	answerList: ["2006", "2009", "2015", "2018"],
+	answer: 1
+},{
+	question: "How many jersey numbers have the Yankees retired?",
+	answerList: ["15", "9", "20", "32"],
+	answer: 0
+},];
